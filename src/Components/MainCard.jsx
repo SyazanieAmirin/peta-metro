8d50d80716c911396d7c068291b6ac614211fdd0
Syazanie <syazamirin@outlook.com>
2024-07-18 02:39:28 +0800

    v1.1.4 + Added a transition for the hover of the github icon in the footer + Added Beijing * Fixed variable height of the images

--- src/Components/MainCard.jsx
+++ src/Components/MainCard.jsx
@@ -1,11 +1,11 @@
 export default function MainCard({ cityName, countryName, image, imageAlt, onClick, continent }) {
     return (
         <div className="bg-secondary rounded-lg transition-all lg:hover:opacity-50 hover:cursor-pointer mt-5" onClick={onClick}>
-            <div className="flex flex-row gap-5">
+            <div className="flex flex-row gap-5 items-center">
                 <div className="rounded-lg">
                     <img src={image} alt={imageAlt} className="h-max-[220px] w-max-[220px] h-[120px] w-[120px] rounded-lg opacity-70" loading="lazy" />
                 </div>
-                <div className="flex flex-col gap-1 py-5">
+                <div className="flex flex-col gap-1 py-5 flex-1">
                     <h1 className="text-xl lg:text-2xl font-bold text-white">{cityName}</h1>
                     <h1 className="lg:text-lg text-white/70">{countryName} | {continent}</h1>
                 </div>
