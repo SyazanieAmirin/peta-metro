0a3a758de3f15426b61d40d1d53242d12b08ee78
Syazanie <syazamirin@outlook.com>
2024-11-02 14:39:17 +0800

    v1.2.1 + Added Dhaka, Minsk, Brussels, Belo Horizonte * Changed Contact Me button to be more visible

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -37,25 +37,26 @@ export default function Contact() {
                 <hr></hr>
             </div>
             <div className="max-w-[1200px] mx-auto mt-10 px-5 mb-10 flex flex-col gap-5">
-                <h1 className="text-white font-bold text-center text-xl">Donation</h1>
-                <p className="text-white text-center"><span className="font-bold">Note:</span> I do not store any of the credit/debit card information as all transactions are processed using Buy Me A Coffee or the Maybank payment processor.</p>
-                <h1 className="text-white font-bold text-center text-l">Choose Type of Donation</h1>
+                <h1 className="text-white font-bold text-center text-2xl">Donation</h1>
                 <div className="flex flex-row gap-5 items-center justify-center">
-                    <button className="bg-primary py-2 px-5 rounded-full text-white font-bold transition-all hover:scale-90" onClick={() => { setIsDonateInternational(true); setIsDonateMalaysia(false) }}>International</button>
-                    <button className="bg-primary py-2 px-5 rounded-full text-white font-bold transition-all hover:scale-90" onClick={() => { setIsDonateInternational(false); setIsDonateMalaysia(true) }}>Malaysia</button>
+                    <button className="bg-blue-500 py-2 px-5 rounded-full text-white font-bold transition-all hover:opacity-70" onClick={() => { setIsDonateInternational(true); setIsDonateMalaysia(false) }}>Using Buy Me A Coffee</button>
+                    <button className="bg-blue-500 py-2 px-5 rounded-full text-white font-bold transition-all hover:opacity-70" onClick={() => { setIsDonateInternational(false); setIsDonateMalaysia(true) }}>Using QR (Malaysians Only!)</button>
                 </div>
                 {isDonateInternational && (
                     <div className="flex flex-col gap-5 items-center">
-                        <p className="text-white text-center">You can donate to me using Buy Me A Coffee. Click the button below to donate to me!</p>
-                        <a href="https://www.buymeacoffee.com/syazanieamirin" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" className="!h-[60px] !w-[217px] transition-all hover:scale-90" /></a>
+                        <p className="text-white text-center">Below is a button for you to donate to me using Buy Me A Coffee. Thank you!</p>
+                        <a href="https://www.buymeacoffee.com/syazanieamirin" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" className="!h-[60px] !w-[217px] transition-all hover:opacity-70" /></a>
+                        <p className="text-white text-center"><span className="font-bold">Note:</span> I do not store any of the credit/debit card information as all transactions are processed using the Buy Me A Coffee payment processor.</p>
                     </div>
                 )}
                 {isDonateMalaysia && (
                     <div className="flex flex-col gap-5 items-center">
                         <img src="../QR_Duit_Now.jpeg" alt="Buy Me A Coffee" className="rounded-lg" />
                         <h1 className="text-white text-center mt-5 text-xl font-bold">Account Number (Maybank): 157157948111</h1>
+                        <p className="text-white text-center"><span className="font-bold">Note:</span> I do not store any of the credit/debit card information as all transactions are processed using the Maybank payment processor.</p>
                     </div>
                 )}
+
             </div>
             <Footer />
         </>
