889e5a12e828d9f5fd08bc2860ccf041c660b0d1
Syazanie <syazamirin@outlook.com>
2024-11-02 10:52:31 +0800

    v1.1.9.2 * Changed footer's link

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-secondary flex flex-row items-center justify-center py-2 w-full mt-10 align-middle gap-2 flex-wrap">
-            <a className="text-white font-bold text-md" href='https://syazanieamirin.github.io/portfolio.github.io/' target='_blank'>Created by <span className='text-blue-200'>Syazanie Amirin | V1.1.9.1</span></a>
+            <a className="text-white font-bold text-md" href='https://syazanie.com' target='_blank'>Created by <span className='text-blue-200'>Syazanie Amirin | V1.1.9.2</span></a>
             <a href="https://github.com/SyazanieAmirin/peta-metro" target='_blank'>
                 <img src="../GithubIcon.svg" alt="Github Logo" className="h-5 w-5 transition-all hover:scale-90 hover:cursor-pointer" />
             </a>
